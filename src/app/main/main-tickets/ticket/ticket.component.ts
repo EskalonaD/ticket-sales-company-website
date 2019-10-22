88b595d4376a0b4474ae68185f475fd7a1a01d50
Denys Eskalona-Lopes <denys_eskalona-lopes@epam.com>
2019-10-22 18:54:41 +0300

    add minor changes to ticket component

--- src/app/main/main-tickets/ticket/ticket.component.ts
+++ src/app/main/main-tickets/ticket/ticket.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RETURNTICKETS } from 'src/app/data/mock-ReturnTickets';
+import { TicketReturn } from 'src/app/data/TicketReturn';
+
+
+
+
 @Component({
   selector: 'app-ticket',
   templateUrl: './ticket.component.html',
   styleUrls: ['./ticket.component.css']
 })
 export class TicketComponent implements OnInit {
-
   constructor() { }
-
+  
+  tickets: TicketReturn[] = RETURNTICKETS;
+  
   ngOnInit() {
+    console.log(this.tickets)
   }
 
 }
