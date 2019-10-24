e8b36ee61170e44d532b12318b9b8939418c0264
Mykola Antypenko <mykola_antypenko@epam.com>
2019-10-24 16:56:29 +0300

    Created range input for main tickets and header for authorization

--- src/app/main/main-tickets/main-tickets.component.ts
+++ src/app/main/main-tickets/main-tickets.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Options, LabelType } from 'ng5-slider';
 @Component({
   selector: 'app-main-tickets',
   templateUrl: './main-tickets.component.html',
   styleUrls: ['./main-tickets.component.css']
 })
-export class MainTicketsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class MainTicketsComponent {
+  minValue1: number = 100;
+  maxValue1: number = 400;
+  minValue2: number = 1;
+  maxValue2: number = 12;
+  options1: Options = {
+    floor: 0,
+    ceil: 12000,
+    translate: (value: number, label: LabelType): string => {
+      switch (label) {
+        case LabelType.Low:
+          return ' $' + value;
+        case LabelType.High:
+          return ' $' + value;
+        default:
+          return '$' + value;
+      }
+  
+    }
+  };
+  options2: Options = {
+    floor: 0,
+    ceil: 48,
+    translate: (value: number, label: LabelType): string => {
+      switch (label) {
+        case LabelType.Low:
+          return value + 'h.';
+        case LabelType.High:
+          return value + 'h.';
+        default:
+          return value + 'h.';
+      }
+    }
+  };
 
 }
